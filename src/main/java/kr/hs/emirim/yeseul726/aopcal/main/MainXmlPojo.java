2b793039b9f0e56a2221cca33296c8400a5ee2ff
Yeseul Lee <cookie726@hanmail.net>
2018-06-22 09:14:15 +0900

    XML 스키마 기반으로 AOP 구현

---
+++ src/main/java/kr/hs/emirim/yeseul726/aopcal/main/MainXmlPojo.java
@@ -0,0 +1,20 @@
+package kr.hs.emirim.yeseul726.aopcal.main;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import kr.hs.emirim.yeseul726.aopcal.cal.Calculator;
+
+public class MainXmlPojo {
+
+	public static void main(String[] args) {
+		GenericXmlApplicationContext ctx =
+				new GenericXmlApplicationContext("classpath:aopPojo.xml");
+		
+		Calculator impCal = ctx.getBean("impCal", Calculator.class);
+		long fiveFact1 = impCal.factorial(5);
+		System.out.println("impCal.factorial(5) = " + fiveFact1);
+		Calculator recCal = ctx.getBean("recCal", Calculator.class);
+		long fiveFact2 = recCal.factorial(5);
+		System.out.println("recCal.factorial(5) = " + fiveFact2);
+	}
+
+}
